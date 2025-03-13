080d429e65a04e88150ac7d82d6756bb9043379d
Renata <renataporto@me.com>
2025-03-13 10:42:24 -0300

    juego basico para analizar cuantas letras lleva tal palabra

---
+++ Cantidad-letras.py
@@ -0,0 +1,4 @@
+#El usuario quiere saber cuantas letras tiene una palabra
+palabra_ingresada = input("por favor ingrese una palabra: ")
+cantidad_letra = len(palabra_ingresada)
+print (f"la cantidad de letras en {palabra_ingresada} es:", cantidad_letra)
