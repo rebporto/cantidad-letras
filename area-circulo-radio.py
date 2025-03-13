a84ff66062655d14afdab57b731f52fb57c569f6
Renata <renataporto@me.com>
2025-03-13 11:06:42 -0300

    como calcular el area del circulo desde el radio

---
+++ area-circulo-radio.py
@@ -0,0 +1,10 @@
+from math import pi
+
+
+solicitud = input("por favor, ingrese el radio del circulo: ")
+radio = float(solicitud)
+
+area=pi*(radio**2)
+
+
+print ("el area del circulo con radio", radio, "es:", area)
