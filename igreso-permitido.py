c3832a74d8ce2315fc37d07589bf381f81d64f49
Renata <renataporto@me.com>
2025-03-13 11:57:41 -0300

    codigo consultando si cliente puede entrar al boliche

---
+++ igreso-permitido.py
@@ -0,0 +1,8 @@
+edad = int(input("por favor, informe su edad: "))
+
+permitido = True if edad >= 18 else False
+
+if edad >= 18:
+    print ("podes entrar")
+else:
+    print ("todavia no tenes edad suficiente")
